c557ad3118802154e0305aea31bf046b146a32ca
ag-tcm <66845101+ag-tcm@users.noreply.github.com>
2021-07-15 09:51:08 -0500

    Update IndexType.java
    Updated IndexType.java to include Manhattan distance.

--- src/main/java/com/spotify/annoy/IndexType.java
+++ src/main/java/com/spotify/annoy/IndexType.java
@@ -1,23 +1,21 @@
 package com.spotify.annoy;
 
 public enum IndexType {
-  ANGULAR(4, 12),
-  EUCLIDEAN(8, 16),
-  DOT(4, 16);
+	ANGULAR(4, 12), EUCLIDEAN(8, 16), DOT(4, 16), MANHATTAN(8, 16);
 
-  private final int offset;
-  private final int kNodeHeaderStyle;
+	private final int offset;
+	private final int kNodeHeaderStyle;
 
-  IndexType(int offset, int kNodeHeaderStyle) {
-    this.offset = offset;
-    this.kNodeHeaderStyle = kNodeHeaderStyle;
-  }
+	IndexType(int offset, int kNodeHeaderStyle) {
+		this.offset = offset;
+		this.kNodeHeaderStyle = kNodeHeaderStyle;
+	}
 
-  public int getOffset() {
-    return offset;
-  }
+	public int getOffset() {
+		return offset;
+	}
 
-  public int getkNodeHeaderStyle() {
-    return kNodeHeaderStyle;
-  }
+	public int getkNodeHeaderStyle() {
+		return kNodeHeaderStyle;
+	}
 }
